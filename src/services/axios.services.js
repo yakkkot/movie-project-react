14022557f548a6e24fd444604bd546102d9eaba6
yakkkot <k0tjeee777@gmail.com>
2022-10-23 18:59:15 +0300

    First

--- src/services/axios.services.js
+++ src/services/axios.services.js
@@ -3,10 +3,8 @@ import {baseURL} from "../configs";
 
 const axiosServices = axios.create({baseURL});
 
-const access = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YTliOTc3NDAzYjRmYzYwYTgwZmY0N2ZiOTJkZGUzYiIsInN1YiI6IjYzNDljODM2NTAxY2YyMDA3YTFlOWY4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.s5L8afa63lCe2W_UFT-vHJEfj8zdA8sEEknqi1piICU';
-
 axiosServices.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${access}`;
+    config.headers.Authorization = `Bearer ${process.env.REACT_APP_ACCESS_ACCESS_TOKEN}`;
     return config;
 });
 
